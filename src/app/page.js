eafc793c2fd416085beb2232f811c543aab85007
KamTheGreen <kamelamichalek@gmail.com>
2025-03-21 09:47:49 +0100

    Test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <main className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold text-blue-500">Zabij sie</h1>
+        <h1 className="text-4xl font-bold text-blue-500">zabij sie</h1>
       </main>
   )
 }
